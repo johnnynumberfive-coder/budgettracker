b981c008fdf963398f41c2c862434e9608db845d
John Nelson <jrnelson75032@gmail.com>
2023-09-17 03:06:44 -0500

    Fixed AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,3 +1,5 @@
+import React, { createContext, useReducer } from 'react';
+
 const AppReducer = (state, action) => {
 	switch (action.type) {
 		default:
@@ -6,7 +8,7 @@ const AppReducer = (state, action) => {
 };
 
 const initialState = {
-	budget: 2097,
+	budget: 2000,
 	expenses: [
 		{ id: 12, name: 'shopping', cost: 40 },
 		{ id: 13, name: 'holiday', cost: 400 },
