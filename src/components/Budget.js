96ead7d22c54e84b07a5880c766703fcb65e4aea
John Nelson <jrnelson75032@gmail.com>
2023-09-17 03:10:42 -0500

    Connect Budget.js to AppContext

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    return (
-        <div className='alert alert-primary'>
-            <span>Budget $2097</span>
-        </div>
-    );
+	const { budget } = useContext(AppContext);
+
+	return (
+		<div className='alert alert-secondary'>
+			<span>Budget: £{budget}</span>
+		</div>
+	);
 };
 
 export default Budget;
