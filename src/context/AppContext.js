807bb0a5a3232104606a373586bbf932fb16ebbb
John Nelson <jrnelson75032@gmail.com>
2023-09-17 03:34:46 -0500

    update reducer

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,11 +2,15 @@ import React, { createContext, useReducer } from 'react';
 
 const AppReducer = (state, action) => {
 	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return {
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
 		default:
 			return state;
 	}
 };
-
 const initialState = {
 	budget: 2097,
 	expenses: [
